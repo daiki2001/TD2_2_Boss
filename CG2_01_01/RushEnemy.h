d2ef307570b8b42f86c119e997faf4c6046a16f3
エグチダイキ <74522674+daiki2001@users.noreply.github.com>
2021-11-18 11:29:47 +0900

    RushEnemyの改良

--- CG2_01_01/RushEnemy.h
+++ CG2_01_01/RushEnemy.h
@@ -8,16 +8,14 @@ public:
 	RushEnemy(Player *player, Vector3 pos, float hp, float N, float e, float r) :
 		BaseEnemy(player, pos, hp, N, e, r, ModelManager::ModelName::TestEnemy) {
 		flame = 0;
-		playerPos = this->pos;
 	}
 
 	void Initialize() override;
 	void Update() override;
-	void RashStart(const Vector3& playerPos);
+	void RushStart();
 
 private:
 	const size_t rushInterval = 60; //突進する間隔
 	size_t flame; //フレーム
-	Vector3 playerPos; //プレイヤーの座標
 
 };
